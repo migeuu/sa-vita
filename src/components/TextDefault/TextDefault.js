81074aef51bcce725452db1fc86f7c8c23a2fef3
migeuu <miguelmarkiori@gmail.com>
2021-11-30 17:36:20 -0300

    TextDefault w/ custom font

---
+++ src/components/TextDefault/TextDefault.js
@@ -0,0 +1,6 @@
+import React from "react";
+import { Text } from "react-native";
+
+export default function TextDefault() {
+  return <Text style={{ fontFamily: "Nunito-Light" }}>some text</Text>;
+}
