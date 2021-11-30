312b7e8346f743a67d7a5b9e264fe8fd417172a0
migeuu <miguelmarkiori@gmail.com>
2021-11-30 16:31:34 -0300

    SafeAreaView > View, CSS PTRN

--- App.js
+++ App.js
@@ -1,19 +1,16 @@
-import React from 'react';
-import { StyleSheet, View } from 'react-native';
+import React from "react";
+import { StyleSheet, SafeAreaView, Platform } from "react-native";
 
 export default function App() {
-  return (
-    <View style={styles.container}>
-      Oi
-    </View>
-  );
+  return <SafeAreaView style={styles.container}></SafeAreaView>;
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
+    paddingTop: Platform.OS === "android" ? 40 : 0,
   },
 });
