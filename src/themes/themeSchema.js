8e1ca549c82636e7ce30179435376041930c8ded
migeuu <miguelmarkiori@gmail.com>
2021-12-07 11:39:00 -0300

    themeSchema +

---
+++ src/themes/themeSchema.js
@@ -0,0 +1,13 @@
+export const themeSchema = {
+  colors: {
+    primary: "#00c4cc",
+    bgLight: "#fdfdfd",
+    bgDark: "#121212",
+    secondary: "#296870",
+    error: "#FF0300",
+  },
+  inputColor: {
+    primary: "#323232",
+    placeholder: "#898989",
+  },
+};
