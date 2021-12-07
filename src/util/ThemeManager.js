7a2735f8d396f3f632365162a49e586dd954bc6d
migeuu <miguelmarkiori@gmail.com>
2021-12-07 11:42:09 -0300

    ThemeManager file and constant

---
+++ src/util/ThemeManager.js
@@ -0,0 +1,22 @@
+import React, { useState } from "react";
+
+export const ThemeContext = React.createContext();
+
+export const ThemeProvider = ({ children }) => {
+  // temas = light, dark
+  const [theme, setTheme] = useState("light");
+
+  const toggleTheme = () => {
+    if (theme === "light") {
+      setTheme("dark");
+    } else {
+      setTheme("light");
+    }
+  };
+
+  return (
+    <ThemeContext.Provider value={{ theme, toggleTheme }}>
+      {children}
+    </ThemeContext.Provider>
+  );
+};
