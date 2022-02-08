dd845209ff010695fd4cfd17aabe5d5b6b510973
migeuu <miguelmarkiori@gmail.com>
2022-02-08 00:16:28 -0300

    Image import + styles

--- src/components/LogoHeader/LogoHeader.js
+++ src/components/LogoHeader/LogoHeader.js
@@ -1,10 +1,16 @@
 import React from "react";
-import { View, Text } from "react-native";
+import { Image } from "react-native";
+import VitaLogo from "../../../assets/images/vita-logo.png";
 
 const LogoHeader = () => {
-  return (
-      <Image source={} style={} />
-  );
+  return <Image source={VitaLogo} style={styles.logoImage} />;
 };
+
+const styles = StyleSheet.create({
+  logoImage: {
+    height: 50,
+    width: 50,
+  },
+});
 
 export default LogoHeader;
