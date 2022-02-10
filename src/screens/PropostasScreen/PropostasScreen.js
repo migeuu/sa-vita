63b0a1ef533702d1bd3eb69086e662f3e04341a3
migeuu <miguelmarkiori@gmail.com>
2022-02-10 15:30:50 -0300

    push screens into migeuu

--- src/screens/PropostasScreen/PropostasScreen.js
+++ src/screens/PropostasScreen/PropostasScreen.js
@@ -1,12 +1,13 @@
-import React from 'react'
-import { View, Text } from 'react-native'
+import React from "react";
+import { View, Text } from "react-native";
 
+// Tela de Propostas
 const PropostasScreen = () => {
   return (
     <View>
       <Text>Tela de Propostas</Text>
     </View>
-  )
-}
+  );
+};
 
-export default PropostasScreen
+export default PropostasScreen;
