3799bb702314800e53aa6be762208cd01a7fa77c
migeuu <miguelmarkiori@gmail.com>
2022-02-17 15:13:09 -0300

    Search Screen +

---
+++ src/screens/SearchScreen/SearchScreen.js
@@ -0,0 +1,14 @@
+import React from "react";
+import { Text } from "react-native";
+import { SafeAreaView } from "react-native-safe-area-context";
+
+// Tela de Propostas
+const PropostasScreen = () => {
+  return (
+    <SafeAreaView>
+      <Text>Tela de Propostas</Text>
+    </SafeAreaView>
+  );
+};
+
+export default PropostasScreen;
