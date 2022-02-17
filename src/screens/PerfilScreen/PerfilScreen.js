7ff26cf25cb3e46e3e6c91e6759f94c16b1530bc
migeuu <miguelmarkiori@gmail.com>
2022-02-17 17:24:53 -0300

    PerfilScreen w/ component

--- src/screens/PerfilScreen/PerfilScreen.js
+++ src/screens/PerfilScreen/PerfilScreen.js
@@ -1,14 +1,25 @@
 //importação de pacotes e componentes
 import React from "react";
-import { Text } from "react-native";
+import { Text, View, StyleSheet } from "react-native";
 import { SafeAreaView } from "react-native-safe-area-context";
+import ProfileHeader from "../../components/ProfileHeader/ProfileHeader";
+import imageBruno from "../../../assets/images/brunin.jpg";
 
 const PerfilScreen = () => {
+  const imageBrunao = imageBruno;
+
   return (
-    <SafeAreaView>
-      <Text>Tela de Perfil</Text>
+    <SafeAreaView style={styles.profileContainer}>
+      <ProfileHeader username="Meguel Marqueore" imageProfile={imageBrunao} />
     </SafeAreaView>
   );
 };
+
+const styles = StyleSheet.create({
+  profileContainer: {
+    flex: 1,
+    backgroundColor: "#c71",
+  },
+});
 
 export default PerfilScreen;
