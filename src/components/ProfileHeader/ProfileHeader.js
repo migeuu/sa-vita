8ea2384f0edaf3a6c0ad3033ce5c2b422c7abf33
migeuu <miguelmarkiori@gmail.com>
2022-02-20 01:09:45 -0300

    Modal + imports + buttons + layout

--- src/components/ProfileHeader/ProfileHeader.js
+++ src/components/ProfileHeader/ProfileHeader.js
@@ -1,28 +1,131 @@
-import { View, Text, StyleSheet, Image, Pressable } from "react-native";
-import React from "react";
-import { MaterialCommunityIcons } from "@expo/vector-icons";
+import {
+  View,
+  Text,
+  StyleSheet,
+  Image,
+  Pressable,
+  Appearance,
+} from "react-native";
+import React, { useState } from "react";
+import { MaterialCommunityIcons, MaterialIcons } from "@expo/vector-icons";
+import ModalCustom from "../Modal/Modal";
+import { useTheme } from "@react-navigation/native";
+
+const colorScheme = Appearance.getColorScheme();
+
+const ModalContentProfile = () => {
+  return (
+    <View style={styles.modalContent}>
+      <Text>MODAL DE PERFIL!</Text>
+      <Text>MODAL DE PERFIL!</Text>
+      <Text>MODAL DE PERFIL!</Text>
+      <Text>MODAL DE PERFIL!</Text>
+      <Text>MODAL DE PERFIL!</Text>
+      <Text>MODAL DE PERFIL!</Text>
+      <Text>MODAL DE PERFIL!</Text>
+      <Text>MODAL DE PERFIL!</Text>
+      <Text>MODAL DE PERFIL!</Text>
+      <Text>MODAL DE PERFIL!</Text>
+      <Text>MODAL DE PERFIL!</Text>
+      <Text>MODAL DE PERFIL!</Text>
+      <Text>MODAL DE PERFIL!</Text>
+      <Text>MODAL DE PERFIL!</Text>
+      <Text>MODAL DE PERFIL!</Text>
+      <Text>MODAL DE PERFIL!</Text>
+      <Text>MODAL DE PERFIL!</Text>
+      <Text>MODAL DE PERFIL!</Text>
+      <Text>MODAL DE PERFIL!</Text>
+    </View>
+  );
+};
 
 const ProfileHeader = (props) => {
+  const openChat = () => {
+    console.log("Abrir chat do id do usuario");
+  };
+
+  const { colors } = useTheme();
+
+  const [isModalVisible, setModalVisible] = useState(false);
+
+  const toggleModal = () => {
+    setModalVisible(!isModalVisible);
+  };
+
   return (
-    <View style={styles.profileHeader}>
-      <View style={styles.profileHeaderTab}>
-        <Text style={styles.profileUsername}>{props.username}</Text>
-        <Pressable>
-          <MaterialCommunityIcons name="cog" size={24} color="black" />
+    <View style={[styles.profileHeader, { backgroundColor: colors.card }]}>
+      <ModalCustom
+        isVisible={isModalVisible}
+        onBackdropPress={toggleModal}
+        onSwipeComplete={toggleModal}
+        children={<ModalContentProfile />}
+      />
+      <View
+        style={[
+          styles.profileHeaderTab,
+          colorScheme == "dark"
+            ? { borderBottomColor: "#222" }
+            : { borderBottomColor: "#e9e9e9" },
+        ]}
+      >
+        <Text style={[styles.profileUsername, { color: colors.text }]}>
+          @{props.username}
+        </Text>
+        <Pressable onPress={toggleModal}>
+          <MaterialCommunityIcons name="cog" size={24} color={colors.text} />
         </Pressable>
       </View>
       <View style={styles.profileInfo}>
-        <Image source={props.imageProfile} style={styles.imageProfile} />
-        <View style={styles.infoDetails}>
+        <View style={styles.imageContainer}>
+          <Image source={props.imageProfile} style={styles.imageProfile} />
+        </View>
+        <View style={styles.infoContainer}>
           <View style={styles.infoContent}>
-            <Text style={styles.infoText}>{props.followers}</Text>
-            <Text style={styles.infoSubtext}>Seguidores</Text>
+            <Text style={[styles.infoText, { color: colors.text }]}>
+              {props.postNumbers}
+            </Text>
+            <Text style={[styles.infoSubtext, { color: colors.text }]}>
+              Publicações
+            </Text>
           </View>
           <View style={styles.infoContent}>
-            <Text style={styles.infoText}>{props.following}</Text>
-            <Text style={styles.infoSubtext}>Seguindo</Text>
+            <Text style={[styles.infoText, { color: colors.text }]}>
+              {props.followers}
+            </Text>
+            <Text style={[styles.infoSubtext, { color: colors.text }]}>
+              Seguidores
+            </Text>
+          </View>
+          <View style={styles.infoContent}>
+            <Text style={[styles.infoText, { color: colors.text }]}>
+              {props.following}
+            </Text>
+            <Text style={[styles.infoSubtext, { color: colors.text }]}>
+              Seguindo
+            </Text>
           </View>
         </View>
+      </View>
+      <View style={styles.buttonContainer}>
+        <Pressable style={styles.followButton} onPress={props.followOnPress}>
+          <Text style={styles.buttonText}>{props.isFollowing}</Text>
+        </Pressable>
+        <Pressable
+          style={[
+            styles.messageButton,
+            colorScheme == "dark"
+              ? { borderColor: "#fff" }
+              : { borderColor: "#000" },
+          ]}
+          onPress={openChat}
+        >
+          <Text style={[styles.buttonText, { color: colors.text }]}>
+            Mensagem
+          </Text>
+        </Pressable>
+        <Pressable style={styles.reportButton}>
+          <MaterialIcons name="report" size={40} color="red" />
+        </Pressable>
       </View>
     </View>
   );
@@ -30,15 +133,14 @@ const ProfileHeader = (props) => {
 
 const styles = StyleSheet.create({
   profileHeader: {
-    backgroundColor: "#f56e90",
-    height: 250,
+    padding: 10,
   },
   profileHeaderTab: {
-    backgroundColor: "#45bd00",
     flexDirection: "row",
     justifyContent: "space-between",
     alignItems: "center",
-    padding: 20,
+    borderBottomWidth: 1,
+    paddingBottom: 10,
   },
   profileUsername: {
     fontSize: 18,
@@ -46,26 +148,68 @@ const styles = StyleSheet.create({
     fontWeight: "bold",
   },
   profileInfo: {
+    paddingVertical: 10,
     flexDirection: "row",
-    justifyContent: "space-between",
+    justifyContent: "center",
     alignItems: "center",
-    padding: 20,
+  },
+  imageContainer: {
+    width: "30%",
   },
   imageProfile: {
     width: 90,
     height: 90,
     borderRadius: 9999,
   },
-  infoDetails: {
-    width: 250,
+  infoContainer: {
+    width: "70%",
     flexDirection: "row",
     justifyContent: "space-around",
+    alignItems: "center",
   },
   infoContent: {
     flexDirection: "column",
     justifyContent: "center",
     alignItems: "center",
-    padding: 10,
+  },
+  infoText: {
+    fontSize: 22,
+  },
+  infoSubtext: {
+    fontSize: 12,
+    fontWeight: "bold",
+  },
+  buttonContainer: {
+    flexDirection: "row",
+    justifyContent: "space-between",
+    alignItems: "center",
+  },
+  followButton: {
+    backgroundColor: "#00c4cc",
+    width: "40%",
+    height: 40,
+    borderRadius: 6,
+    justifyContent: "center",
+    alignItems: "center",
+  },
+  messageButton: {
+    borderWidth: 1.5,
+    width: "43%",
+    height: 40,
+    borderRadius: 6,
+    justifyContent: "center",
+    alignItems: "center",
+  },
+  buttonText: {
+    fontSize: 16,
+    fontWeight: "400",
+  },
+  reportButton: {
+    width: 40,
+    height: 40,
+    marginRight: 5,
+    justifyContent: "center",
+    alignItems: "center",
   },
 });
 
