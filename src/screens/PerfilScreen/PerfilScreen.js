36f66a61f2a6c621163bc4d27f061d1828b205e9
migeuu <miguelmarkiori@gmail.com>
2022-02-20 01:10:34 -0300

    new props + ISSUE state

--- src/screens/PerfilScreen/PerfilScreen.js
+++ src/screens/PerfilScreen/PerfilScreen.js
@@ -1,16 +1,38 @@
 //importação de pacotes e componentes
-import React from "react";
+import React, { useState } from "react";
 import { Text, View, StyleSheet } from "react-native";
 import { SafeAreaView } from "react-native-safe-area-context";
 import ProfileHeader from "../../components/ProfileHeader/ProfileHeader";
 import imageBruno from "../../../assets/images/brunin.jpg";
 
 const PerfilScreen = () => {
+  // Tenho que criar um estado que seja booleano e que retorne um texto caso seja true e outro caso seja false
   const imageBrunao = imageBruno;
+
+  const [isFollowing, setIsFollowing] = useState(false);
+  const [followText, setFollowText] = useState("Seguir");
+
+  // ISSUE - O estado do componente FOLLOW_TEXT so atualiza quando é clicado pela segunda vez
+  const toggleIsFollowing = () => {
+    setIsFollowing(!isFollowing);
+    if (isFollowing === true) {
+      setFollowText("Seguindo");
+    } else if (isFollowing === false) {
+      setFollowText("Seguir");
+    }
+  };
 
   return (
     <SafeAreaView style={styles.profileContainer}>
-      <ProfileHeader username="Bruno Ecker" imageProfile={imageBrunao} followers={1578} following={550} />
+      <ProfileHeader
+        username="Bruno_Ecker"
+        imageProfile={imageBrunao}
+        followers={1578}
+        following={550}
+        postNumbers={32}
+        isFollowing={followText}
+        followOnPress={toggleIsFollowing}
+      />
     </SafeAreaView>
   );
 };
@@ -18,7 +40,6 @@ const PerfilScreen = () => {
 const styles = StyleSheet.create({
   profileContainer: {
     flex: 1,
-    backgroundColor: "#c71",
   },
 });
 
