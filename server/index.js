3a1cf1383db0d75eb26e07449303ae9525c86132
niconogueira <nicolasgnogueira@gmail.com>
2022-02-25 17:43:17 -0600

    Queries funcionando

--- server/index.js
+++ server/index.js
@@ -1,12 +1,135 @@
+// const express = require ('express')
+// const bodyParser = require('body-parser')
+// const app = express()
+// const cors = require("cors");
+// const db = require('./queries')
+// const PORT = 3000
+
+
+// app.use(cors())
+// app.use(bodyParser.json())
+// app.use(
+//     bodyParser.urlencoded({
+//         extended:true,
+//     })
+// )
+
+
+
+
+// app.get('/', (request, response) => {
+//     response.json({info: 'API NodeJS com PostgreSQL.'})
+// })
+
+// app.get('/perfil', db.getUsers)
+// app.get('/perfil/:id', db.getUserById)
+// app.post('/perfil', db.createUser)
+// app.put('/perfil/:id', db.updateUser)
+// app.delete('/perfil/:id', db.deleteUser)
+
+
+// app.listen(PORT, ()=> {
+//     console.log(`Servidor inicializado em http://localhost:${PORT}`)
+// })
+
+
+
+
+
+
+// iniciando o server
 const express = require("express");
-const bodyParser = require("body-parser");
 const app = express();
-const db = require("./queries");
+const cors = require("cors");
+const pool = require("./queries");
 const PORT = 5000;
 
-app.use(bodyParser.json());
-app.use(
-  bodyParser.urlencoded({
-    extended: true,
-  })
-);
+// uso de middleware
+app.use(cors());
+app.use(express.json());
+
+// Rotas //
+
+// Create anuncio
+
+// app.post("/anuncios", async (req, res) => {
+//   try {
+//     const { title, description, date_start, date_end, category, email, phone } =
+//       req.body;
+//     const newAnuncio = await pool.query(
+//       "INSERT INTO anuncios (title, description, date_start, date_end, category, email, phone) VALUES($1, $2, $3, $4, $5, $6, $7) RETURNING *",
+//       [title, description, date_start, date_end, category, email, phone]
+//     );
+//     res.json(newAnuncio);
+//   } catch (err) {
+//     console.error(err.message);
+//   }
+// });
+
+// // Get todos anuncios
+
+app.get("/perfil", async (req, res) => {
+  try {
+    const allPerfil = await pool.query("SELECT * FROM perfil");
+    res.json(allPerfil.rows);
+  } catch (err) {
+    console.error(err.message);
+  }
+});
+
+// Get em um anuncio unico
+
+// app.get("anuncios/:id", async (req, res) => {
+//   try {
+//     const { id } = req.params;
+//     const anuncio = await pool.query(
+//       "SELECT * FROM anuncios WHERE idanuncio = $1",
+//       [id]
+//     );
+
+//     res.json(anuncio.rows[0]);
+//   } catch (err) {
+//     console.error(err.message);
+//   }
+// });
+
+// // Update em um anuncio unico
+
+// app.put("anuncios/:id", async (req, res) => {
+//   try {
+//     const { id } = req.params;
+//     const { title, description, date_start, date_end, category, email, phone } =
+//       req.body;
+
+//     const updateAnuncio = await pool.query(
+//       "UPDATE anuncios SET title = $1, description = $2, date_start = $3, date_end = $4, category = $5, email = $6, phone = $7 WHERE idanuncio = $8 ",
+//       [title, description, date_start, date_end, category, email, phone, id]
+//     );
+
+//     res.json("Anúncio atualizado!");
+//   } catch (err) {
+//     console.error(err.message);
+//   }
+// });
+
+// // Deletar anuncio
+
+// app.delete("anuncios/:id", async (req, res) => {
+//   try {
+//     const { id } = req.params;
+//     const deleteAnuncio = await pool.query(
+//       "DELETE FROM anuncios WHERE idanuncio = $1",
+//       [id]
+//     );
+
+//     res.json("Anúncio excluído!");
+//   } catch (err) {
+//     console.error(err.message);
+//   }
+// });
+
+// server methods
+app.listen(PORT, () => {
+  console.log(`Server has started in: http://localhost:${PORT}`);
+});
+
--- server/queries.js
+++ server/queries.js
@@ -1,30 +1,98 @@
+// const Pool = require("pg").Pool;
+// const pool = new Pool({
+//   host: "localhost",
+//   database: "VitaDB",
+//   user: "postgres",
+//   password: "aluno",
+//   port: 5432,
+// });
+
+// const getUsers = async (req, res) => {
+//   try {
+//     const allUsers = await pool.query("SELECT * FROM perfil");
+//     response.json(allUsers);
+//   } catch (err) {
+//     console.error(err.message);
+//   }
+// };
+
+// const getUserById = (req, response) => {
+//   const id = parseInt(req.params.id);
+//   pool.query("SELECT * FROM PERFIL WHERE ID = $1", [id], (error, results) => {
+//     if (error) {
+//       throw error;
+//     }
+//     response.status(200).json(results.rows);
+//   });
+// };
+
+// const createUser = (req, response) => {
+//   const { nome, email } = req.body;
+
+//   pool.query(
+//     "INSERT INTO PERFIL (NOME. EMAIL) VALUES ($1, $2)",
+//     [NOME, EMAIL],
+//     (error, results) => {
+//       if (error) {
+//         throw error;
+//       }
+//       response
+//         .status(201)
+//         .send(`Usuário adicionado com ID: ${results.insertId}`);
+//     }
+//   );
+// };
+
+// const updateUser = (req, response) => {
+//   const { nome, email } = req.body;
+//   const id = parseInt(req.params.id);
+
+//   pool.query(
+//     "UPDATE PERFIL SET NOME = $1, EMAIL = $2 WHERE ID = $3",
+//     [nome, email, id],
+//     (error, results) => {
+//       if (error) {
+//         throw error;
+//       }
+
+//       response.status(200).send(`Usuário com ID: ${id} modificado com sucesso`);
+//     }
+//   );
+// };
+
+// const deleteUser = (req, response) => {
+//   const id = parseInt(req.params.id);
+
+//   pool.query("DELETE FROM PERFIL WHERE ID = $1", [id], (error, results) => {
+//     if (error) {
+//       throw error;
+//     }
+//     response.status(201).send(`Usuário com ID ${id} deletado com sucesso`);
+//   });
+// };
+
+// module.exports = {
+//   getUsers,
+//   getUserById,
+//   createUser,
+//   updateUser,
+//   deleteUser,
+// };
 
 
-const axios = require("axios")
+
+
 
 const Pool = require("pg").Pool;
+
 const pool = new Pool({
-  host: "localhost",
-  database: "vita_db",
   user: "postgres",
   password: "senai",
+  host: "localhost",
   port: 5432,
+  database: "DBVita",
 });
 
-const api = axios.create({
-  baseURL: `http://10.3.62.62:3000/`
-})
+module.exports = pool;
 
-console.log(api);
 
-module.exports = [api, pool];
-
-// import qs from 'qs';
-// const data = { 'bar': 123 };
-// const options = {
-//   method: 'POST',
-//   headers: { 'content-type': 'application/x-www-form-urlencoded' },
-//   data: qs.stringify(data),
-//   url,
-// };
-// axios(options);
